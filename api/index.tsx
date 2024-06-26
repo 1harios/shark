07c54ce4054411a91448d8fbebf12669222e3dea
kKarandash <155941723+kKarandash@users.noreply.github.com>
2024-06-26 16:29:38 +0300

    Add files via upload

---
+++ api/index.tsx
@@ -0,0 +1,147 @@
+import { Button, Frog } from 'frog'
+import { handle } from 'frog/vercel'
+import { Box, Image, VStack, vars } from "../lib/ui.js";
+import { randomInt } from 'crypto';
+
+// Uncomment this packages to tested on local server
+// import { devtools } from 'frog/dev';
+// import { serveStatic } from 'frog/serve-static';
+
+const baseUrl = "https://warpcast.com/~/compose";
+const text = "Find out whatkind of shark you are 🦈\n\n▶️ Press start.\n\nJoin the Shark Mafia community - /shark\n\n✏️ Frame by @harios";
+const embedUrl = "https://sharks-by-harios.vercel.app/api/frame";
+
+const BROWSER_LOCATION = `${baseUrl}?text=${encodeURIComponent(text)}&embeds[]=${encodeURIComponent(embedUrl)}`;
+
+export const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api/frame',
+  imageAspectRatio: '1:1',
+  imageOptions: {
+    height: 1024,
+    width: 1024,
+  },
+  ui: { vars },
+  browserLocation: BROWSER_LOCATION
+})
+
+// Array of image URLs with aspect ratio 1.22:1
+const images = [
+  { id: '1', url: '/images/img1.png' },
+  { id: '2', url: '/images/img2.png' },
+  { id: '3', url: '/images/img3.png' },
+  { id: '4', url: '/images/img4.png' },
+  { id: '5', url: '/images/img5.png' },
+  { id: '6', url: '/images/img6.png' },
+  { id: '7', url: '/images/img7.png' },
+  { id: '8', url: '/images/img8.png' },
+  { id: '9', url: '/images/img9.png' },
+  { id: '10', url: '/images/img10.png' },
+  { id: '11', url: '/images/img11.png' },
+  { id: '12', url: '/images/img12.png' },
+  { id: '13', url: '/images/img13.png' },
+  { id: '14', url: '/images/img14.png' },
+  { id: '15', url: '/images/img15.png' },
+  { id: '16', url: '/images/img16.png' },
+  { id: '17', url: '/images/img17.png' },
+  { id: '18', url: '/images/img18.png' },
+];
+
+app.frame('/', (c) => {
+  return c.res({
+    title: 'Find out whatkind of shark you are 🦈',
+    image: '/Main.gif',
+    intents: [
+      <Button action="/button-pressed">▶️ Start ◀️</Button>,
+    ],
+  })
+});
+
+app.frame('/button-pressed', (c) => {
+  const randomIndex = randomInt(images.length);
+  const selectedImage = images[randomIndex];
+
+  const baseUrl = "https://warpcast.com/~/compose";
+  const text = "Find out whatkind of shark you are 🦈\n\n▶️ Press start.\n\nJoin the Shark Mafia community - /shark\n\n✏️ Frame by @harios";
+  const embedUrlByUser = `https://sharks-by-harios.vercel.app/api/frame/shared/${selectedImage.id}`;
+
+  const SHARE_BY_USER = `${baseUrl}?text=${encodeURIComponent(text)}&embeds[]=${encodeURIComponent(embedUrlByUser)}`;
+
+  return c.res({
+    title: 'Find out whatkind of shark you are 🦈',
+    image: (
+      <Box
+        grow
+        alignVertical="center"
+        backgroundColor="black"
+        height="100%"
+      >
+        <VStack gap="4">
+          <Box
+            grow
+            alignVertical="center"
+            backgroundColor="white"
+            padding="40"
+            height="100%"
+          >
+            <Image
+              height="100%"
+              width="100%"
+              objectFit="contain"
+              src={selectedImage.url}
+            />
+          </Box>
+        </VStack>
+      </Box>
+    ),
+    intents: [
+      <Button action="/">🔄 Try again</Button>,
+      <Button.Link href={SHARE_BY_USER}>↪️ Share</Button.Link>,
+    ],
+  });
+});
+
+app.frame('/shared/:imageId', (c) => {
+  const imageId = c.req.param('imageId');
+  const selectedImage = images.find(img => img.id === imageId)?.url || '/Main.gif';
+
+  return c.res({
+    title: 'Find out whatkind of shark you are 🦈',
+    image: (
+      <Box
+        grow
+        alignVertical="center"
+        backgroundColor="black"
+        height="100%"
+      >
+        <VStack gap="4">
+          <Box
+            grow
+            alignVertical="center"
+            backgroundColor="white"
+            padding="40"
+            height="100%"
+          >
+            <Image
+              height="100%"
+              width="100%"
+              objectFit="contain"
+              src={selectedImage}
+            />
+          </Box>
+        </VStack>
+      </Box>
+    ),
+    intents: [
+      <Button action="/">🔄 Try again</Button>,
+      <Button.Link href={BROWSER_LOCATION}>↪️ Share</Button.Link>,
+    ],
+  });
+});
+
+
+// Uncomment this line code to tested on local server
+// devtools(app, { serveStatic });
+
+export const GET = handle(app)
+export const POST = handle(app)
